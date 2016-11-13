52418d8ec253457fa9f97863dade6c15d3a62aa6
Matheus Cardoso <matheus.mcas@gmail.com>
2016-11-12 23:15:27 -0300

    added google places search

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,26 +1,30 @@
+import { Place } from './place.model';
+import { GooglePlacesService } from './../../providers/google-places.service';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import localForage from "localforage";
-import {GoogleGeocoding} from '../../providers/google-geocoding'
-import {CustomLocation} from './location';
+import {GoogleGeocoding} from '../../providers/google-geocoding.service'
+import {CustomLocation} from './location.model';
 import 'rxjs/add/operator/finally';
 
 @Component({
   selector: 'page-page1',
   templateUrl: 'page1.html',
-  providers : [GoogleGeocoding]
+  providers : [GoogleGeocoding, GooglePlacesService]
 })
 export class Page1 {
 
   private location: CustomLocation;
   private city_name: String;
+  private places: Array<Place>;
 
   constructor(
-    public navCtrl: NavController, 
-    public geocodingService: GoogleGeocoding
+    public navCtrl: NavController,
+    public geocodingService: GoogleGeocoding,
+    public placesService: GooglePlacesService
   ) {
     this.location = new CustomLocation();
-    this.get_geolocation(); 
+    this.get_geolocation();
   }
 
   set_location(pos: Position){
@@ -31,23 +35,25 @@ export class Page1 {
   }
 
   get_geolocation(){
-    
+
     if (!navigator.geolocation){
       this.city_name = "Geolocation is not supported by your browser";
       return;
     }
-    
+
     let self = this;
-    localForage.getItem('location', function(err, value){
+    localForage.getItem('location', function(err, value: CustomLocation){
       if(value === null){
         navigator.geolocation.getCurrentPosition((position) =>{
           self.set_location(position)
           self.get_geo_city_name();
+          self.searchPlaces(self.location.lat, self.location.long);
         }, () => {
           self.location.city_name = "Unable to retrieve your location";
         });
       }else{
         self.location = value;
+        self.searchPlaces(self.location.lat, self.location.long);
       }
     });
   }
@@ -64,6 +70,20 @@ export class Page1 {
       },
       () => {
         localForage.setItem('location', self.location);
+      }
+    )
+  }
+
+  searchPlaces(lat, lng, radius=500){
+    let self = this;
+    this.placesService.nearbySearch(lat, lng, radius)
+    .subscribe(
+      results => {
+        self.places = results;
+      },
+      err => {
+        self.places = [];
+        console.log(err);
       }
     )
   }
--- src/providers/google-places.service.ts
+++ src/providers/google-places.service.ts
@@ -20,32 +20,35 @@ export class GooglePlacesService{
   BASE_URL: string;
 
   constructor(public http: Http) {
-    this.BASE_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?rankby=distance";
+    this.BASE_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
   }
 
   nearbySearch(lat: Number, lng: Number, radius:Number): Observable<any>{
 
-    let url = `${this.BASE_URL}&location=${lat},${lng}&radius=${radius}&types=pharmacy&key=${API_KEY}`;
+    let url = `${this.BASE_URL}?location=${lat},${lng}&radius=${radius}&types=pharmacy&key=${API_KEY}`;
     console.log(url);
 
-    return this.http.get(url)
-                .map(this.extract_data)
-                .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    return this.http.get(url).map(this.extract_data);
   }
 
   private extract_data(res: Response){
     let body = res.json();
-    let place = new Place();
-    place.id = body.results.id;
-    place.location.lat = body.results.geometry.location.lat
-    place.location.lng = body.results.geometry.location.lng
-    place.icon = body.results.icon;
-    place.name = body.results.name;
-    place.photo_reference = body.results.photos[0].photo_reference;
-    place.address = body.results.vicinity;
-    place.place_id = body.results.place_id;
-    console.log(place)
-    return place
+    let results = []
+    for(let result of body.results){
+      let place = new Place();
+      place.id = result.id;
+      place.location.lat = result.geometry.location.lat
+      place.location.lng = result.geometry.location.lng
+      place.icon = result.icon;
+      place.name = result.name;
+      if("photos" in result){
+        place.photo_reference = result.photos[0].photo_reference;
+      }
+      place.address = result.vicinity;
+      place.place_id = result.place_id;
+      results.push(place)
+    }
+    return results
   }
 
 }
